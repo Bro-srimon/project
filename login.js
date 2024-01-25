39149de95ecd5a3368d7982c2be70bebe0b7b269
Bro-srimon <139974774+Bro-srimon@users.noreply.github.com>
2024-01-25 18:36:34 +0530

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-function Login() {
-    player_name = document.getElementById("playername").ariaValueMax;
-    localStorage.setItem("player_name", player_name);
-
-    window.location = "gamepage.html";
-}
